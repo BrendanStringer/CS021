1d06e2882fa5af87d039c3fda9ed8b53afb151ad
BrendanStringer <bstringe@uvm.edu>
2015-04-19 14:41:20 -0400

    delete old files

--- Assignment 1.0/Assignment 1.0.py
+++
@@ -1,7 +0,0 @@
-print('My name is Brendan Stringer.')
-print('I have a BS in Civil Engineering.')
-print('I work for UVM full time managing the UVM Tech Team alongside Travis Bartlett.')
-print('Our staff work at the UVM Computer Carry-In Clinic.')	
-print('I practice Aikido several days a week.')
-print('I have one brother, Ashten, who also attends UVM.')
-print('He is studying Global Studies.')
